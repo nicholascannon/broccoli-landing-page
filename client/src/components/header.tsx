d66406b6317aa3f6ca0740afc5345c504a586611
nicholascannon <nicholascannon1@gmail.com>
2023-05-23 20:09:47 +0800

    Simple header + footer components

---
+++ client/src/components/header.tsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css';
+
+export const Header = () => {
+    return (
+        <header className={styles.header}>
+            <span>BROCOLI & CO.</span>
+        </header>
+    );
+};
