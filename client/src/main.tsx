dcc008a42a75cc9117d6543254da8a55f8611eb5
nicholascannon <nicholascannon1@gmail.com>
2023-05-23 19:16:33 +0800

    Add modern CSS reset

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
+import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
     <React.StrictMode>
