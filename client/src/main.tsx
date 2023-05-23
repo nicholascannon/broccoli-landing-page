46a863d89ea99646de1aff31b46a7d96a2055a7d
nicholascannon <nicholascannon1@gmail.com>
2023-05-23 20:09:59 +0800

    Make app full height

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { LandingPage } from './pages/landing';
 
 import './reset.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
     <React.StrictMode>
-        <h1>Hello, world!</h1>
+        <LandingPage />
     </React.StrictMode>
 );
