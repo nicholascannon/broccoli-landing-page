bac141c3ac649901b26dbba00d2561e25c85ceea
nicholascannon <nicholascannon1@gmail.com>
2023-05-23 20:17:47 +0800

    Update footer content

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,5 +1,10 @@
 import styles from './footer.module.css';
 
 export const Footer = () => {
-    return <footer className={styles.footer}>Footer</footer>;
+    return (
+        <footer className={styles.footer}>
+            <p>&copy; 2023 Broccoli &amp; Co. All rights reserved.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </footer>
+    );
 };
