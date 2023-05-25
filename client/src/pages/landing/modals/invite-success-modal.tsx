8f9fdca64955ee455c119162111c28d92a8f366c
nicholascannon <nicholascannon1@gmail.com>
2023-05-25 18:56:54 +0800

    Simple invite success modal

---
+++ client/src/pages/landing/modals/invite-success-modal.tsx
@@ -0,0 +1,16 @@
+import { GenericModalView } from '../../../modals/generic-modal-view';
+import { ModalComponent } from '../../../modals/modal-engine';
+
+import styles from './invite-success-modal.module.css';
+
+export const InviteSuccessModal: ModalComponent = (props) => {
+    return (
+        <GenericModalView>
+            <div className={styles.content}>
+                <h2>All done!</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, explicabo.</p>
+                <button onClick={props.closeModal}>ok</button>
+            </div>
+        </GenericModalView>
+    );
+};
