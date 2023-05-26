70ec0fa0df2d8e268dbed2966f23bf4bda991d9a
nicholascannon <nicholascannon1@gmail.com>
2023-05-26 15:36:49 +0800

    Fix copy

--- client/src/common/components/footer.tsx
+++ client/src/common/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 export const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>Made with &hearts; in Melbourne</p>
+            <p>Made with &hearts; in Melbourne.</p>
             <p>&copy; 2023 Broccoli &amp; Co. All rights reserved.</p>
         </footer>
     );
