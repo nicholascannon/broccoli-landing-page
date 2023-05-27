8623e8dc27f69eaeb4f32155a8717061319ba6d5
nicholascannon <nicholascannon1@gmail.com>
2023-05-27 17:16:51 +0800

    Add responsive padding via variables

--- client/src/common/components/header.tsx
+++ client/src/common/components/header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.css';
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <span>BROCOLI & CO.</span>
+            <span className={styles.title}>BROCOLI & CO.</span>
         </header>
     );
 };
