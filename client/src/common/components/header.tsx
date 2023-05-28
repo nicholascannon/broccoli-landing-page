ddf50e3dd7ce25f8dbbbe64f94aa0105c1c4ffc5
nicholascannon <nicholascannon1@gmail.com>
2023-05-28 18:36:29 +0800

    fix header typo

--- client/src/common/components/header.tsx
+++ client/src/common/components/header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.css';
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <span className={styles.title}>BROCOLI & CO.</span>
+            <span className={styles.title}>BROCCOLI & CO.</span>
         </header>
     );
 };
