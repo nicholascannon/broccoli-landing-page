dd029071edc184d80549b11d7f08967681ab7d8b
nicholascannon <nicholascannon1@gmail.com>
2023-07-05 20:00:13 +0800

    Refactor modal engine to use useReducer

--- client/src/common/modals/modal-engine.tsx
+++ client/src/common/modals/modal-engine.tsx
@@ -1,33 +1,20 @@
-import { FunctionComponent, ReactNode, createContext, useCallback, useContext, useEffect, useState } from 'react';
+import { Dispatch, FunctionComponent, ReactNode, createContext, useContext, useEffect, useReducer } from 'react';
 import { createPortal } from 'react-dom';
 import { useOutsideClickObserver } from '../hooks/use-outside-click-observer';
 import { ModalOverlay } from './components/modal-overlay';
 
 export const ModalEngine = ({ children }: { children: ReactNode }) => {
-    const [modals, setModals] = useState<ModalComponent[]>([]);
-
-    const closeCurrentModal = useCallback(() => {
-        setModals((modals) => modals.slice(1));
-    }, []);
-
-    const showModal: QueueModalFunction = useCallback((modal) => {
-        setModals((modals) => [modal, ...modals]);
-    }, []);
-
-    const replaceAndShowModal: QueueModalFunction = useCallback((modal) => {
-        setModals((modals) => [modal, ...modals.slice(1)]);
-    }, []);
-
+    const [modals, dispatch] = useReducer(modalReducer, []);
     const CurrentModal = modals.length ? modals[0] : undefined;
 
     return (
-        <ModalEngineContext.Provider value={{ showModal, replaceAndShowModal, closeCurrentModal }}>
+        <ModalEngineContext.Provider value={dispatch}>
             {children}
 
             {CurrentModal &&
                 createPortal(
                     <ModalOverlay>
-                        <CurrentModal closeModal={closeCurrentModal} />
+                        <CurrentModal closeModal={() => dispatch({ type: 'CLOSE' })} />
                     </ModalOverlay>,
                     document.getElementById(MODAL_PORTAL_ID) as HTMLElement
                 )}
@@ -35,21 +22,48 @@ export const ModalEngine = ({ children }: { children: ReactNode }) => {
     );
 };
 
+const modalReducer = (state: State, action: ModalAction): State => {
+    switch (action.type) {
+        case 'CLOSE':
+            return state.slice(1);
+        case 'REPLACE':
+            return [action.modal, ...state.slice(1)];
+        case 'SHOW':
+            return [action.modal, ...state];
+        default:
+            return state;
+    }
+};
+
+type State = ModalComponent[];
+
 export type ModalComponent = FunctionComponent<{ closeModal: () => void }>;
+
+type ModalAction =
+    | { type: 'SHOW'; modal: ModalComponent }
+    | { type: 'REPLACE'; modal: ModalComponent }
+    | { type: 'CLOSE' };
+
+const ModalEngineContext = createContext<Dispatch<ModalAction>>(() => {
+    // empty
+});
+
+export const useShowModal = (): QueueModalFunction => {
+    const dispatch = useContext(ModalEngineContext);
+    return (modal) => dispatch({ type: 'SHOW', modal });
+};
+
+export const useReplaceAndShowModal = (): QueueModalFunction => {
+    const dispatch = useContext(ModalEngineContext);
+    return (modal) => dispatch({ type: 'REPLACE', modal });
+};
 
 export type QueueModalFunction = (modal: ModalComponent) => void;
 
-const ModalEngineContext = createContext<ModalEngineState>({} as ModalEngineState);
-
-type ModalEngineState = {
-    showModal: QueueModalFunction;
-    replaceAndShowModal: QueueModalFunction;
-    closeCurrentModal: () => void;
+export const useCloseCurrentModal = () => {
+    const dispatch = useContext(ModalEngineContext);
+    return () => dispatch({ type: 'CLOSE' });
 };
-
-export const useShowModal = () => useContext(ModalEngineContext).showModal;
-export const useReplaceAndShowModal = () => useContext(ModalEngineContext).replaceAndShowModal;
-export const useCloseCurrentModal = () => useContext(ModalEngineContext).closeCurrentModal;
 
 /**
  * Includes standard UX to allow a user to close the modal if they hit ESCAPE
